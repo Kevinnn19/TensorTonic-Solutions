f1ecbe01c65aef1b8193bc8405907c6896aa1a37
KEVIN ARANIYA <159253308+Kevinnn19@users.noreply.github.com>
2026-04-05 09:31:07 +0530

    Update solution for label-smoothing-loss

--- label-smoothing-loss/label-smoothing-loss.py
+++ label-smoothing-loss/label-smoothing-loss.py
@@ -5,13 +5,15 @@ def label_smoothing_loss(predictions, target, epsilon):
     Compute cross-entropy loss with label smoothing.
     """
     # Write code here
+    pred = np.asarray(predictions, dtype = float)
+    pred = np.clip(pred, 1e-9, 1.0)
     q = []
-    pred = np.asarray(predictions)
     K = pred.size
     for i in range(K) :
         if i == target :
             q.append((1 - epsilon) + (epsilon / K))
         else :
             q.append(epsilon / K)
+    q = np.asarray(q)
     loss = -np.sum(q * np.log(pred))
     return loss
